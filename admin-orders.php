9d1618f583d1568eab3007d763c68c0ec6517c2c
Flavio Lima <flaviolima.s@live.com>
2018-11-09 18:00:59 -0200

    Search admin Orders incompleto

--- admin-orders.php
+++ admin-orders.php
@@ -88,14 +88,46 @@ $app->get("/admin/orders/:idorder", function($idorder){
 
 });
 
-$app->get("/admin/orders", function(){
+$app->get("/admin/orders/:idorder/status", function($idorder){
 
 	User::verifyLogin();
 
+	$search = (isset($_GET['search'])) ? $_GET['search'] : "";
+
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
+	if($search != ''){
+
+	$pagination = Order::getPageSearch($search, $page);
+
+
+	}else {
+
+	$pagination = Order::getPage($page);
+		
+	}
+
+	$pages = [];
+
+	for($x = 0; $x < $pagination['pages']; $x++)
+	{
+
+		array_push($pages, [
+			'href'=>'/admin/orders?'.http_build_query([
+				'page'=>$x+1,
+				'search'=>$search
+			]),
+			'text'=>$x+1
+		]);
+
+	}
+
 	$page = new PageAdmin();
 
-	$page->setTpl("orders", [
-			"orders"=>Order::listAll()
+	$page->setTpl("order-status", [
+			"orders"=>$pagination['data'],
+			"search"=>$search,
+			"pages"=>$pages
 		]
 	);
 
